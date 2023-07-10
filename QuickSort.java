42f0fe378df8c3e8ba96ffbb16a635c2171a7a62
Eugen Korschun <117730070+Nimgvae@users.noreply.github.com>
2023-07-10 13:18:15 +0200

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,44 @@
+package HW_1107;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (array[j] < pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 2, 9, 1, 6, 3};
+        quickSort(array);
+        System.out.println("Sorted array: " + Arrays.toString(array));
+    }
+}
